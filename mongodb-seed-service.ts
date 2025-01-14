b40888e6a2326f5de077a62db36f12fef251294e
NomadNiko <nomad.niko.inc@gmail.com>
2025-01-13 23:09:17 -1000

    build: fixing shema mappings

--- mongodb-seed-service.ts
+++ mongodb-seed-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { VendorSchemaClass, VendorStatusEnum } from './entities/vendor.schema';
+import { VendorSchemaClass, VendorStatusEnum } from './src/vendors/infrastructure/persistence/document/entities/vendor.schema';
 
 @Injectable()
 export class VendorSeedService {
